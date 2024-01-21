168c691d3a78b6e11d1e5748d3e838808aa45155
Sam Shanks <shanks.sam@outlook.com>
2024-01-21 20:26:28 +0000

    feat: ai questions

---
+++ src/commands/ai/ask.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { OpenAI } = require('openai')
+
+const openai = new OpenAI({apiKey: process.env.OPENAI_KEY});
+
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ask')
+		.setDescription('Ask a question')
+        .addStringOption(option =>
+            option.setName("question")
+                .setDescription("Replies with an answer")
+                .setRequired(true)
+            )
+		.setDMPermission(false),
+	async execute(interaction) {
+
+
+
+
+
+        const question = interaction.options.getString('question') 
+        console.log(question)
+
+        const completion = await openai.chat.completions.create({
+            messages: [{ role: "user", content: question }],
+            model: "gpt-3.5-turbo",
+          });
+        
+          console.log(completion.choices[0]);
+
+		await interaction.reply(completion.choices[0]);
+	},
+};
